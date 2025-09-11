715d252344fa65765a0cff98a0ec70a27b42bf99
Miguel Angel <miguelangelgarb@gmail.com>
2025-09-11 18:26:04 -0500

    Component login and styles home

--- academy/src/app/features/login/login.ts
+++ academy/src/app/features/login/login.ts
@@ -1,3 +1,6 @@
+import { Solidbutton } from './../../shared/components/buttons/solidbutton/solidbutton';
+import { CommonModule } from '@angular/common';
+import { Card } from './../../shared/components/card/card';
 import { Footer } from '@shared/components/footer/footer';
 import { Component } from '@angular/core';
 import { Header } from '@shared/components/header/header';
@@ -5,10 +8,12 @@ import { Header } from '@shared/components/header/header';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [Header, Footer],
+  imports: [Header, Footer, Card, CommonModule, Solidbutton],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export default class Login {
+  username: string = '';
+  password: string = '';
 
 }
